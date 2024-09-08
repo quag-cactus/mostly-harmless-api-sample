9146a245ffdb12f83fafcc28a8c35a806da32747
quag-cactus <49008834+quag-cactus@users.noreply.github.com>
2024-09-08 22:50:22 +0900

    test: Add test codes for getting schedules

--- tests/services/test_quaggy_manger.py
+++ tests/services/test_quaggy_manger.py
@@ -1,3 +1,7 @@
+import datetime
+
+import pytest
+
 from src.services import quaggy_manager
 
 
@@ -11,3 +15,28 @@ def test_check_quaggy_status(mocker):
     # mock_quaggy_manager.check_quaggy.return_value = False
     # result = mock_quaggy_manager.check_quaggy()
     # assert result == False
+
+
+@pytest.mark.parametrize(
+    "quaggy_id, now_time, origin_expected_behavior, expected_behavior",
+    [
+        (142, datetime.datetime.now(), "walk", "walk"),
+        (
+            142,
+            datetime.datetime.now() + datetime.timedelta(hours=1),
+            "free",
+            "undesided",
+        ),
+        (142, datetime.datetime.now() + datetime.timedelta(hours=-1), "free", "free"),
+        (142, datetime.datetime.now() + datetime.timedelta(hours=-1), None, None),
+    ],
+)
+def test_get_quaggy_schedule(
+    mocker, quaggy_id, now_time, origin_expected_behavior, expected_behavior
+):
+
+    mock_class = mocker.patch("src.services.quaggy_manager.QuaggyWatcher")
+    mock_class.return_value.get_schedule.return_value = origin_expected_behavior
+
+    behavior = quaggy_manager.get_quaggy_schedule(quaggy_id, now_time)
+    assert behavior == expected_behavior
