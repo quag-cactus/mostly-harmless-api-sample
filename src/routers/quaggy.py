d579c6cfda64c3b3d0ec96605474c830a3ca36e0
quag-cactus <49008834+quag-cactus@users.noreply.github.com>
2024-09-08 23:07:15 +0900

    feat: Implement getting schedules api

--- src/routers/quaggy.py
+++ src/routers/quaggy.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import APIRouter
 from fastapi_versionizer.versionizer import api_version
 from asgi_correlation_id import correlation_id
@@ -5,7 +7,7 @@ from loguru import logger
 
 from src.services import quaggy_manager
 
-router = APIRouter(prefix="/quaggy", tags=["quaggy"])
+router = APIRouter(prefix="/quaggy", tags=["quaggy"], redirect_slashes=False)
 
 
 @api_version(1)
@@ -13,4 +15,11 @@ router = APIRouter(prefix="/quaggy", tags=["quaggy"])
 def check_status(quaggy_id):
     logger.info(f"Checking status of quaggy with id: {quaggy_id}")
     s = quaggy_manager.check_quaggy_status(quaggy_id)
+    return {"request_id": correlation_id.get(), "status": s}
+
+@api_version(2)
+@router.get("/get_schedule_now/{quaggy_id}")
+def get_schedule_now(quaggy_id):
+    now_time = datetime.datetime.now()
+    s = quaggy_manager.get_quaggy_schedule(quaggy_id, now_time)
     return {"request_id": correlation_id.get(), "status": s}
--- src/services/quaggy_manager.py
+++ src/services/quaggy_manager.py
@@ -1,5 +1,5 @@
 import random
-
+import datetime
 from loguru import logger
 
 
@@ -21,6 +21,20 @@ class QuaggyWatcher:
         else:
             return "walking"
 
+    def get_schedule(self, target_time: datetime.datetime):
+
+        random_value = random.random()
+        logger.debug(f"value: {random_value}")
+
+        if random_value < 0.25:
+            return "sleep"
+        elif random_value < 0.5:
+            return "eat"
+        elif random_value < 0.75:
+            return "play"
+        else:
+            return "free"
+
     def feed(self, food):
         if food == "fish":
             self.food = food
@@ -35,3 +49,16 @@ def check_quaggy_status(quaggy_id):
     watcher = QuaggyWatcher(quaggy_id)
     behavior = watcher.check_status()
     return f"Quaggy(ID: {quaggy_id}) is {behavior} now."
+
+
+def get_quaggy_schedule(quaggy_id: int, target_time: datetime.datetime):
+
+    now_time = datetime.datetime.now()
+
+    watcher = QuaggyWatcher(quaggy_id)
+    behavior = watcher.get_schedule(target_time)
+
+    if behavior == "free":
+        behavior = "free" if target_time < now_time else "undesided"
+
+    return behavior
