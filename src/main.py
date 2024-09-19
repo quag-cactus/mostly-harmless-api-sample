40722959f289ebd2ed9618e6373804b6fb095b1b
quag-cactus <49008834+quag-cactus@users.noreply.github.com>
2024-09-19 20:07:32 +0900

    feat: Support dynamic version displaying in swagger docs

--- src/main.py
+++ src/main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI, HTTPException
-import uvicorn
+from fastapi.openapi.utils import get_openapi
 
 from asgi_correlation_id import CorrelationIdMiddleware, correlation_id
 from loguru import logger
 from fastapi_versionizer.versionizer import Versionizer
 
+from version import __version__
 from src.error import ApiError
 from src.routers import quaggy
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version=__version__,
+        summary="This is a very custom OpenAPI schema",
+        description="Here's a longer description of the custom **OpenAPI** schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 logger.add("./log/log_info.log", rotation="1MB", retention=3, level="INFO")
 logger.add("./log/log_err.log", rotation="1MB", retention=3, level="ERROR")
@@ -17,9 +35,11 @@ app.add_middleware(CorrelationIdMiddleware)
 
 app.include_router(quaggy.router)
 
+
 @app.exception_handler(ApiError)
 def api_error_handler(request, err: ApiError):
-    raise HTTPException(status_code=err.status_code, detail=f'{err.detail}')
+    raise HTTPException(status_code=err.status_code, detail=f"{err.detail}")
+
 
 @app.get("/")
 async def root():
@@ -36,8 +56,6 @@ async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
-
-
 versions = Versionizer(
     app=app,
     prefix_format="/v{major}",
@@ -45,6 +63,8 @@ versions = Versionizer(
     latest_prefix="/latest",
     sort_routes=True,
 ).versionize()
+
+app.openapi = custom_openapi
 
 # if __name__ == "__main__":
 #    uvicorn.run("main:app", port=8000, reload=True)
